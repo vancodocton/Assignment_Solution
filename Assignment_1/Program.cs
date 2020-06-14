f38d5295e84bad2b0926dbc8f2d7d6a25d6dbf23
vancodocton <user.email>
2020-06-14 18:03:03 +0700

    new version

--- Assignment_1/Program.cs
+++ Assignment_1/Program.cs
@@ -7,13 +7,13 @@ namespace Assignment_1
    {
       static void Main()
       {
-         List<int> nums = new List<int>();
+         List<int> grades = new List<int>();
          int userChoice;
          do
          {
             DisplayMenu();
             userChoice = GetUserChoice();
-            ExecuteOption(nums, userChoice);
+            ExecuteOption(grades, userChoice);
          } while (userChoice != 0);
 
       }
@@ -21,12 +21,12 @@ namespace Assignment_1
       private static void DisplayMenu()
       {
          Console.WriteLine("===================================================");
-         Console.WriteLine("    Finding biggest numbers in the integer array");
+         Console.WriteLine("    Finding student(s) having the highest grade");
          Console.WriteLine("===================================================");
-         Console.WriteLine(" Press 1 to add a number");
-         Console.WriteLine(" Press 2 to add numbers");
-         Console.WriteLine(" Press 3 to display all numbers");
-         Console.WriteLine(" Press 4 to display the biggest number(s)");
+         Console.WriteLine(" Press 1 to add a student's grade");
+         Console.WriteLine(" Press 2 to add a number of students' grade");
+         Console.WriteLine(" Press 3 to display all student's grade entered");
+         Console.WriteLine(" Press 4 to display the highest grade");
          Console.WriteLine(" Press 0 to exit the program");
          Console.WriteLine("===================================================");
          return;
@@ -41,69 +41,72 @@ namespace Assignment_1
          }
          return GetUserChoice();
       }
-      private static int EnterNumber()
+      private static int EnterStudent()
       {
-         Console.Write("Enter a number: ");
+         Console.Write("Enter student's grade [0 -> 100]: ");
 
          if (int.TryParse(Console.ReadLine(), out int res))
          {
-            return res;
+            if (0 <= res && res <= 100)
+               return res;
          }
-         return EnterNumber();
+         return EnterStudent();
       }
       private static int EnterQuantity()
       {
-         Console.Write("Enter the number of numbers: ");
+         Console.Write("Enter the number of students [1 -> 10]: ");
 
          if (int.TryParse(Console.ReadLine(), out int res))
          {
-            return res;
+            if (0 < res && res <= 10)
+               return res;
          }
          return EnterQuantity();
       }
-      private static int EnterNumber(int index)
+      private static int EnterStudent(int index)
       {
-         Console.Write($"Enter the {index} number: ");
+         Console.Write($"Enter the {index} student's grade [0 -> 100]: ");
 
          if (int.TryParse(Console.ReadLine(), out int res))
          {
-            return res;
+            if (0 <= res && res <= 100)
+               return res;
          }
-         return EnterNumber(index);
+         return EnterStudent(index);
       }
-      private static void EnterManyNumbers(List<int> nums)
+      private static void EnterManyStudents(List<int> grades)
       {
          int n = EnterQuantity();
          for (int i = 1; i <= n; i++)
          {
-            nums.Add(EnterNumber(i));
+            grades.Add(EnterStudent(i));
          }
       }
-      private static void DisplayNums(List<int> nums)
+      private static void DisplayStudents(List<int> grades)
       {
-         if (nums.Count == 0)
+         if (grades.Count == 0)
          {
             Console.WriteLine("There's nothing entered");
             return;
          }
          Console.WriteLine("| {0,10} | {1,10} |", "NO", "Value");
-         for (int i = 0; i < nums.Count; i++)
+         for (int i = 0; i < grades.Count; i++)
          {
-            Console.WriteLine("| {0,10} | {1,10} |", i + 1, nums[i]);
+            Console.WriteLine("| {0,10} | {1,10} |", i + 1, grades[i]);
          }
          return;
       }
-      private static void DisplayMaxNums(List<int> nums, List<int> index)
+      private static void DisplayMaxGradeIndex(List<int> grades, List<int> index)
       {
          Console.WriteLine("| {0,10} | {0,10} |", "NO", "ID");
          for (int i = 0; i < index.Count; i++)
          {
-            Console.WriteLine("| {0,10} | {1,10} |", i + 1, nums[index[i]]);
+            Console.WriteLine("| {0,10} | {1,10} |", i + 1, grades[index[i]]);
          }
       }
-      private static void DisplayBiggestNums(List<int> nums)
+      private static void DisplayMaxGrade(List<int> grades)
       {
-         if (nums.Count == 0)
+         if (grades.Count == 0)
          {
             Console.WriteLine("There's nothing entered");
             return;
@@ -111,21 +114,21 @@ namespace Assignment_1
          List<int> index = new List<int>();
          int maxNum = int.MinValue;
 
-         for (int i = 0; i < nums.Count; i++)
+         for (int i = 0; i < grades.Count; i++)
          {
-            if (nums[i] > maxNum)
+            if (grades[i] > maxNum)
             {
-               maxNum = nums[i];
+               maxNum = grades[i];
                index = new List<int> { i };
             }
-            else if (nums[i] == maxNum)
+            else if (grades[i] == maxNum)
             {
                index.Add(i);
             }
          }
 
          Console.WriteLine("The highest grade: {0}", maxNum);
-         DisplayMaxNums(nums, index);
+         DisplayMaxGradeIndex(grades, index);
          return;
       }
       private static void DisplayExitMessage()
@@ -133,21 +136,21 @@ namespace Assignment_1
          Console.Write("Thanks for using. Press any key to exit...");
          _ = Console.ReadKey();
       }
-      private static void ExecuteOption(List<int> nums, int userChoice)
+      private static void ExecuteOption(List<int> grades, int userChoice)
       {
          switch (userChoice)
          {
             case 1:
-               nums.Add(EnterNumber());
+               grades.Add(EnterStudent());
                break;
             case 2:
-               EnterManyNumbers(nums);
+               EnterManyStudents(grades);
                break;
             case 3:
-               DisplayNums(nums);
+               DisplayStudents(grades);
                break;
             case 4:
-               DisplayBiggestNums(nums);
+               DisplayMaxGrade(grades);
                break;
             case 0:
                DisplayExitMessage();
