858e044dc23e3713a20448adf9557fe47046d6cb
vancodocton <user.email>
2020-06-30 18:07:30 +0700

    update class Utils

---
+++ Assignment_2/Utils.cs
@@ -0,0 +1,11 @@
+﻿namespace Assignment_2
+{
+   public class Utils
+   {
+      internal static string MAX_STRING_LINE { get; private set; }
+      internal Utils()
+      {
+         MAX_STRING_LINE = "50";
+      }
+   }
+}
